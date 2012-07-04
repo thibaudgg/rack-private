f549d2a10848b3e9f234ce7fa8c2c3afaa06857e
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-07-04 11:33:39 +0200

    Fix symbole downcase issue

--- lib/rack/private.rb
+++ lib/rack/private.rb
@@ -1,6 +1,6 @@
 module Rack
   class Private
-    
+
     def initialize(app, options = {}, &block)
       @app = app
       @options = options
@@ -12,13 +12,13 @@ module Rack
 
       instance_eval(&block) if block_given?
     end
-    
+
     def call(env)
       request = Rack::Request.new(env)
-      
+
       # Check code in session and return Rails call if is valid
       return @app.call(env) if (already_auth?(request) || public_page?(request))
-      
+
       # If post method check :code_param value
       if request.post? && code_valid?(request.params["private_code"])
         request.session[:private_code] = request.params["private_code"]
@@ -28,7 +28,7 @@ module Rack
         render_private_form
       end
     end
-    
+
   private
     # Render staging html file
     def render_private_form
@@ -36,20 +36,20 @@ module Rack
         ::File.open(html_template, 'rb').read
       ]]
     end
-    
+
     def html_template
       @options[:template_path] || ::File.expand_path('../private/index.html', __FILE__)
     end
-    
+
     # Validate code
     def code_valid?(code)
       [@options[:code] || @options[:codes]].flatten.include?(code)
     end
-    
+
     def already_auth?(request)
       code_valid?(request.session[:private_code])
     end
-    
+
     # Checks if the url matches one of our exception strings or regexs
     def public_page?(request)
       @exceptions.find {|exception| exception?(request, exception)}
@@ -66,7 +66,7 @@ module Rack
 
     def matches_method?(request, method)
       return true if method.nil?
-      request.request_method.downcase == method.values.first.downcase.to_s
+      request.request_method.to_s.downcase == method.values.first.to_s.downcase
     end
 
     def except(match, method = nil)
